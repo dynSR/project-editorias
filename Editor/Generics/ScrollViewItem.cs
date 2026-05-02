92a9601f5b27c2329c4e2403014b4a400bf4ad14
dynSR <dylan.schumacker+github@outlook.fr>
2026-05-02 15:07:02 +0200

    ScrollViewItem.cs made abstract + added builder

--- Editor/Generics/ScrollViewItem.cs
+++ Editor/Generics/ScrollViewItem.cs
@@ -1,54 +1,23 @@
 ﻿using System;
 using UnityEditor;
-using UnityEngine;
+using Utilitas;
 
 namespace Editorias {
-    public class ScrollViewItem : IScrollViewItem, ISelectable {
-        public string Guid { get; }
-        public string Name { get; }
+    public abstract class ScrollViewItem : IScrollViewItem, ISelectable {
+        public string Guid { get; protected set; }
+        public string Name { get; protected set; }
 
-        public bool CanBeSelected { get; }
+        public bool CanBeSelected { get; protected set; }
         public bool IsSelected { get; protected set; }
-        public Action<ISelectable> OnSelection { get; set; } = delegate { };
 
-        protected readonly SelectableButton SelectableButton;
-
-        public ScrollViewItem(string guid, string name, bool canBeSelected = true, bool isSelected = false) {
-            Guid = guid;
-            Name = name;
-            CanBeSelected = canBeSelected;
-            IsSelected = isSelected;
-
-            SelectableButton = new SelectableButton.Builder()
-                .WithText($"{Name}")
-                .WithAction(Toggle)
-                .Build();
-        }
-
-        public virtual void Draw() {
-            EditorGUI.BeginDisabledGroup(!CanBeSelected);
-            SelectableButton.Draw();
-            EditorGUI.EndDisabledGroup();
-        }
+        public abstract void Draw();
 
         public virtual void Toggle() {
             if (!IsSelected) Select();
             else Deselect();
-
-            Debug.Log("TOGGLED");
         }
-
-        public virtual void Select() {
-            IsSelected = true;
-            OnSelection?.Invoke(this);
-            SelectableButton.Select();
-        }
-
-        public virtual void Deselect() {
-            IsSelected = false;
-            OnSelection?.Invoke(this);
-            SelectableButton.Deselect();
-        }
+        public virtual void Select() => IsSelected = true;
+        public virtual void Deselect() => IsSelected = false;
 
         public virtual int CompareTo(object other) {
             if (other is not ScrollViewItem otherItem) {
@@ -63,8 +32,42 @@ namespace Editorias {
             return string.Compare(Guid, otherItem.Guid, StringComparison.Ordinal);
         }
 
-        public virtual void Destroy() {
-            SelectableButton.OnClick -= Toggle;
+        public class Builder<T> where T : ScrollViewItem, new() {
+            private bool hasCanBeSelectedFlagBeenSet;
+            private bool hasSelectedFlagBeenSet;
+            protected T Item { get; } = new();
+
+            public Builder<T> WithGuid(string guid) {
+                Item.Guid = !guid.IsNullOrWhiteSpace() ? guid : System.Guid.NewGuid().ToString();
+                return this;
+            }
+
+            public Builder<T> WithName(string name) {
+                Item.Name = !name.IsNullOrWhiteSpace() ? name : "Unassigned Name";
+                return this;
+            }
+
+
+            public Builder<T> CanBeSelected(bool value) {
+                hasCanBeSelectedFlagBeenSet = true;
+                Item.CanBeSelected = value;
+                return this;
+            }
+
+            public Builder<T> IsSelected(bool value) {
+                hasSelectedFlagBeenSet = true;
+                Item.IsSelected = value;
+                return this;
+            }
+
+            public ScrollViewItem Build() {
+                if (Item.Guid.IsNullOrWhiteSpace()) Item.Guid = System.Guid.NewGuid().ToString();
+                if (Item.Name.IsNullOrWhiteSpace()) Item.Name = "Unassigned Name";
+                if (!hasCanBeSelectedFlagBeenSet) Item.CanBeSelected = true;
+                if (!hasSelectedFlagBeenSet) Item.IsSelected = false;
+
+                return Item;
+            }
         }
     }
 }
